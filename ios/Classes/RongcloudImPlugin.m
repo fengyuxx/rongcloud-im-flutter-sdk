0d630e047c9a87e897808dcc7e31b00253c582b1
elvin <fengyuxx223@gmail.com>
2019-09-03 19:56:50 +0800

    增加融云推送token设置

--- ios/Classes/RongcloudImPlugin.m
+++ ios/Classes/RongcloudImPlugin.m
@@ -1,18 +1,28 @@
 #import "RongcloudImPlugin.h"
 #import "RCIMFlutterWrapper.h"
+#import <RongIMLib/RongIMLib.h>
 
 @implementation RongcloudImPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"rongcloud_im_plugin"
-            binaryMessenger:[registrar messenger]];
-  RongcloudImPlugin* instance = [[RongcloudImPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
-  [[RCIMFlutterWrapper sharedWrapper] addFlutterChannel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"rongcloud_im_plugin"
+                                     binaryMessenger:[registrar messenger]];
+    RongcloudImPlugin* instance = [[RongcloudImPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+    [registrar addApplicationDelegate:instance];
+    [[RCIMFlutterWrapper sharedWrapper] addFlutterChannel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
     [[RCIMFlutterWrapper sharedWrapper] handleMethodCall:call result:result];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSString *token = [NSString stringWithFormat:@"%@", deviceToken];
+    token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
+    token = [token stringByReplacingOccurrencesOfString:@"<" withString:@""];
+    token = [token stringByReplacingOccurrencesOfString:@">" withString:@""];
+    [[RCIMClient sharedRCIMClient] setDeviceToken:token];
 }
 
 @end
